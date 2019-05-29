381ebc74efdd6609c99997cf56bd69937c2d9f92
Michael Zion <noisleahcim@gmail.com>
2019-05-29 10:46:10 +0300

    iterate issues returned from issues_fetcher

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from services.IssuesParser import IssuesParser
 
 def main():
     projects_finder = ProjectsFinder()
-    issues_parser = IssuesParser()
     issues_fetcher = IssuesFetcher()
+    issues_parser = IssuesParser()
 
     python_repos = projects_finder.search_python_repos()
 
     for repo in python_repos:
-        issues_fetcher.get_issues_from_github(repo)
+        issues = issues_fetcher.get_issues_from_github(repo)
 
-    for issue in issues:
-        parser.parse_issue(issue)
+        for issue in issues:
+            issues_parser.parse_issue(issue)
 
 if __name__ == '__main__':
     main()
