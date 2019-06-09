1dcf296893075ba455c5c927a9f68de02be0a3fd
DorElias <DorElias1@gmail.com>
2019-06-09 22:29:12 +0300

    basic parse issues

--- services/IssuesParser.py
+++ services/IssuesParser.py
@@ -1,3 +1,11 @@
 class IssuesParser:
     def parse_issue(self, issue):
-        pass
+        result = []
+        if not issue.pull_request:
+            return
+        pr = issue.as_pull_request()
+        files = pr.get_files()
+        for file in files:
+            result.append({'name': file.filename, 'patch': file.patch})
+        return result
+
