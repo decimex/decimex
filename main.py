5de14092104c5e1f6a83f78fde45f0dbac72609f
Michael Zion <noisleahcim@gmail.com>
2019-06-29 16:49:47 +0300

    add a counter to prevent reaching github's quota

--- main.py
+++ main.py
@@ -11,22 +11,27 @@ def main():
     host = "localhost"
     port = "5432"
     db_name = "issues"
+
     projects_finder = ProjectsFinder()
     issues_fetcher = IssuesFetcher()
+
     issues_db = IssuesDb(db_type, user_name, password, host, port, db_name)
     issues_db.create_defined_tables()
+
     issues_parser = IssuesParser(issues_db)
 
     python_repos = projects_finder.search_python_repos()
+
+    counter = 0
 
     for repo in python_repos:
         issues = issues_fetcher.get_issues_from_github(repo)
 
         for issue in issues:
             issues_parser.parse_issue(issue)
-            break
-
-        break
+            counter += 1
+            if counter >= 4:
+                break
 
 
 if __name__ == '__main__':
